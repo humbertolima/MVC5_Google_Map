ef56bf0168c68b2d558559e652e9ecd0ce9a654a
Humberto Lima <hlg870415@hotmail.com>
2017-02-08 00:45:28 -0500

    Add project files.

---
+++ MapDemo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MapDemo.Models;
+
+namespace MapDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        AllCountriesEntities db = new AllCountriesEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult Search(string postal_code)
+        {
+            place place = db.places.Where(x => x.postal_code.Equals(postal_code)).FirstOrDefault();
+            return Json(place, JsonRequestBehavior.AllowGet);
+        }
+    }
+    
+}
---
+++ MapDemo/scripts/Map.js
@@ -0,0 +1,25 @@
+﻿var map;
+var startLatLng = { lat: 25.8965, lng: -80.1570 };
+
+function startMap(LatLng) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: LatLng,
+        zoom: 14
+    });
+    var marker = new google.maps.Marker({
+        position: LatLng,
+        map: map
+    });
+}
+function initMap()
+{
+    startMap(startLatLng);
+}
+function search(postal_code) {
+    var url = "Home/Search";
+    $.getJSON(url, { postal_code: postal_code }, function (data) {
+        startMap({ lat: data.latitude, lng: data.longitude });
+    });
+
+}
+
